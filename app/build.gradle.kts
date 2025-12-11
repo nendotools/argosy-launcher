b4fc721522c4d1a12cb10acda413884b0480e5fb
nendo <tmgast@gmail.com>
2025-12-12 02:36:40 +0900

    Bump version to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 39
-        versionName = "0.8.1"
+        versionName = "0.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
